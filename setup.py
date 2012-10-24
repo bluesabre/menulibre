1b0353735ef01bbac5d57aa47f64385e3e1b9c5c
Sean Davis <smd.seandavis@gmail.com>
2012-10-24 07:34:25 -0400

    Upgraded to latest quickly, package ready for quantal.

--- setup.py
+++ setup.py
@@ -27,12 +27,13 @@ except ImportError:
     sys.exit(1)
 assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
 
-def update_config(values = {}):
+def update_config(libdir, values = {}):
 
+    filename = os.path.join(libdir, 'menulibre_lib/menulibreconfig.py')
     oldvalues = {}
     try:
-        fin = file('menulibre_lib/menulibreconfig.py', 'r')
-        fout = file(fin.name + '.new', 'w')
+        fin = file(filename, 'r')
+        fout = file(filename + '.new', 'w')
 
         for line in fin:
             fields = line.split(' = ') # Separate variable from value
@@ -46,18 +47,87 @@ def update_config(values = {}):
         fin.close()
         os.rename(fout.name, fin.name)
     except (OSError, IOError), e:
-        print ("ERROR: Can't find menulibre_lib/menulibreconfig.py")
+        print ("ERROR: Can't find %s" % filename)
         sys.exit(1)
     return oldvalues
 
+
+def move_desktop_file(root, target_data, prefix):
+    # The desktop file is rightly installed into install_data.  But it should
+    # always really be installed into prefix, because while we can install
+    # normal data files anywhere we want, the desktop file needs to exist in
+    # the main system to be found.  Only actually useful for /opt installs.
+
+    old_desktop_path = os.path.normpath(root + target_data +
+                                        '/share/applications')
+    old_desktop_file = old_desktop_path + '/menulibre.desktop'
+    desktop_path = os.path.normpath(root + prefix + '/share/applications')
+    desktop_file = desktop_path + '/menulibre.desktop'
+
+    if not os.path.exists(old_desktop_file):
+        print ("ERROR: Can't find", old_desktop_file)
+        sys.exit(1)
+    elif target_data != prefix + '/':
+        # This is an /opt install, so rename desktop file to use extras-
+        desktop_file = desktop_path + '/extras-menulibre.desktop'
+        try:
+            os.makedirs(desktop_path)
+            os.rename(old_desktop_file, desktop_file)
+            os.rmdir(old_desktop_path)
+        except OSError as e:
+            print ("ERROR: Can't rename", old_desktop_file, ":", e)
+            sys.exit(1)
+
+    return desktop_file
+
+def update_desktop_file(filename, target_pkgdata, target_scripts):
+
+    try:
+        fin = file(filename, 'r')
+        fout = file(filename + '.new', 'w')
+
+        for line in fin:
+            if 'Icon=' in line:
+                line = "Icon=%s\n" % (target_pkgdata + 'media/menulibre.svg')
+            elif 'Exec=' in line:
+                cmd = line.split("=")[1].split(None, 1)
+                line = "Exec=%s" % (target_scripts + 'menulibre')
+                if len(cmd) > 1:
+                    line += " %s" % cmd[1].strip()  # Add script arguments back
+                line += "\n"
+            fout.write(line)
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find %s" % filename)
+        sys.exit(1)
+
+def compile_schemas(root, target_data):
+    if target_data == '/usr/':
+        return  # /usr paths don't need this, they will be handled by dpkg
+    schemadir = os.path.normpath(root + target_data + 'share/glib-2.0/schemas')
+    if (os.path.isdir(schemadir) and
+            os.path.isfile('/usr/bin/glib-compile-schemas')):
+        os.system('/usr/bin/glib-compile-schemas "%s"' % schemadir)
+
+
 class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
     def run(self):
-        values = {'__menulibre_data_directory__': "'%s'" % (self.prefix + '/share/menulibre/'),
-                  '__version__': "'%s'" % self.distribution.get_version()}
-        previous_values = update_config(values)
         DistUtilsExtra.auto.install_auto.run(self)
-        update_config(previous_values)
 
+        target_data = '/' + os.path.relpath(self.install_data, self.root) + '/'
+        target_pkgdata = target_data + 'share/menulibre/'
+        target_scripts = '/' + os.path.relpath(self.install_scripts, self.root) + '/'
+
+        values = {'__menulibre_data_directory__': "'%s'" % (target_pkgdata),
+                  '__version__': "'%s'" % self.distribution.get_version()}
+        update_config(self.install_lib, values)
+
+        desktop_file = move_desktop_file(self.root, target_data, self.prefix)
+        update_desktop_file(desktop_file, target_pkgdata, target_scripts)
+        compile_schemas(self.root, target_data)
 
         
 ##################################################################################
