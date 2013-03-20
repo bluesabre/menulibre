06ec47dd497682045fb16f4987be61bb2c935956
Sean Davis <smd.seandavis@gmail.com>
2013-03-20 19:01:17 -0400

    Update license

--- menulibre_lib/Window.py
+++ menulibre_lib/Window.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-# Copyright (C) 2012 Sean Davis <smd.seandavis@gmail.com>
+# Copyright (C) 2012-2013 Sean Davis <smd.seandavis@gmail.com>
 # This program is free software: you can redistribute it and/or modify it 
 # under the terms of the GNU General Public License version 3, as published 
 # by the Free Software Foundation.
