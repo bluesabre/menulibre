d319a41a838964e9139873dde404b833682e518c
Sean Davis <smd.seandavis@gmail.com>
2013-03-20 21:53:57 -0400

    confirmar antes de publicar

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='menulibre',
-    version='13.03.4',
+    version='13.03.5',
     license='GPL-3',
     author='Sean Davis',
     author_email='smd.seandavis@gmail.com',
