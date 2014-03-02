03880ae6f935857ae7e46d38387d6e35e3fe8263
Sean Davis <smd.seandavis@gmail.com>
2014-03-02 08:34:18 -0500

    Update NEWS, increment version number

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='menulibre',
-    version='2.0.1',
+    version='2.0.2',
     license='GPL-3',
     author='Sean Davis',
     author_email='smd.seandavis@gmail.com',
