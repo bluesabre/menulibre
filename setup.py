a1537d56795bb7d2de093dec62d40c6fd08382af
Sean Davis <smd.seandavis@gmail.com>
2015-09-20 16:36:26 -0400

    Bump version number and update release notes

--- setup.py
+++ setup.py
@@ -182,7 +182,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='menulibre',
-    version='2.1.0',
+    version='2.1.1',
     license='GPL-3',
     author='Sean Davis',
     author_email='smd.seandavis@gmail.com',
