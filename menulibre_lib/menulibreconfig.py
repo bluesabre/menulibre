6b1d5aa093f91818818373a982cbaebf85ef5c7b
Sean Davis <smd.seandavis@gmail.com>
2018-01-18 06:30:16 -0500

    Release 2.1.4

--- menulibre_lib/menulibreconfig.py
+++ menulibre_lib/menulibreconfig.py
@@ -25,7 +25,7 @@ __all__ = [
 # files). By default, this is ../data, relative your trunk layout
 __menulibre_data_directory__ = '../data/'
 __license__ = 'GPL-3'
-__version__ = '2.1.3'
+__version__ = '2.1.4'
 
 import os
 
