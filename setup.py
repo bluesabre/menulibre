a188ea54b66fa2a1fe6a5db99ae2550bc17f5695
Sean Davis <smd.seandavis@gmail.com>
2018-02-05 06:20:38 -0500

    Release 2.1.5

--- setup.py
+++ setup.py
@@ -188,7 +188,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='menulibre',
-    version='2.1.4',
+    version='2.1.5',
     license='GPL-3',
     author='Sean Davis',
     author_email='smd.seandavis@gmail.com',
