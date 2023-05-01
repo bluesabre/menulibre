e60a3b2a612b2264646a0f83575164a2fc2f4866
Sean Davis <sean@bluesabre.org>
2023-04-30 22:02:41 -0400

    Release 2.3.1

--- setup.py
+++ setup.py
@@ -229,7 +229,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='menulibre',
-    version='2.3.0',
+    version='2.3.1',
     license='GPL-3',
     author='Sean Davis',
     author_email='sean@bluesabre.org',
