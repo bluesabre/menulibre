6f06dc6de5e0d1fbf11518362bb81e420402d63d
Sean Davis <sean@bluesabre.org>
2023-08-01 06:51:55 -0400

    Improve delete label UX

--- menulibre/MenulibreApplication.py
+++ menulibre/MenulibreApplication.py
@@ -378,7 +378,7 @@ class MenulibreWindow(Gtk.ApplicationWindow):
 
         self.revert_button = headerbar.add_button("document-revert-symbolic", _("Revert"))
         self.execute_button = headerbar.add_button("media-playback-start-symbolic", _("Test Launcher"))
-        self.delete_button = headerbar.add_button("edit-delete-symbolic", _("Delete"))
+        self.delete_button = headerbar.add_button("edit-delete-symbolic", _("Delete..."))
 
         headerbar.add_search(self.search_box)
 
@@ -592,7 +592,7 @@ class MenulibreWindow(Gtk.ApplicationWindow):
 
         toolbar.add_separator()
 
-        self.delete_button = toolbar.add_button("edit-delete", _("Delete"))
+        self.delete_button = toolbar.add_button("edit-delete", _("Delete..."))
 
         separator = toolbar.add_separator()
         separator.set_draw(False)
@@ -948,8 +948,8 @@ class MenulibreWindow(Gtk.ApplicationWindow):
                     self.actions[name].set_sensitive(True)
 
             # Enable deletion (LP: #1751616)
-            self.delete_button.set_sensitive(True)
-            self.delete_button.set_tooltip_text("")
+            #self.delete_button.set_sensitive(True)
+            #self.delete_button.set_tooltip_text(_("Delete..."))
 
         # If the entry has a query...
         else:
@@ -972,7 +972,7 @@ class MenulibreWindow(Gtk.ApplicationWindow):
 
             # Disable deletion (LP: #1751616)
             self.delete_button.set_sensitive(False)
-            self.delete_button.set_tooltip_text("")
+            self.delete_button.set_tooltip_text(_("Delete..."))
 
     def on_search_cleared(self, widget, event, user_data=None):
         """Generic search cleared callback function."""
@@ -984,7 +984,7 @@ class MenulibreWindow(Gtk.ApplicationWindow):
         # Since the filename has changed, check if it is now writable...
         if filename is None or os.access(filename, os.W_OK):
             self.delete_button.set_sensitive(True)
-            self.delete_button.set_tooltip_text("")
+            self.delete_button.set_tooltip_text(_("Delete..."))
         else:
             self.delete_button.set_sensitive(False)
             self.delete_button.set_tooltip_text(
@@ -996,7 +996,7 @@ class MenulibreWindow(Gtk.ApplicationWindow):
         # Disable deletion if we're in search mode (LP: #1751616)
         if self.search_box.get_text() != "":
             self.delete_button.set_sensitive(False)
-            self.delete_button.set_tooltip_text("")
+            self.delete_button.set_tooltip_text(_("You cannot delete this file while a search is active."))
 
     def get_inner_value(self, key):
         """Get the value stored for key."""
